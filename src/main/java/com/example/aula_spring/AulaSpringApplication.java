84ec6c4225c92687cfe3d477fde79cd882e2e591
Wellyson Martins <wellysonmartins@gmail.com>
2019-05-29 22:58:05 -0300

    Spring boot project

---
+++ src/main/java/com/example/aula_spring/AulaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.aula_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringApplication.class, args);
+	}
+
+}
